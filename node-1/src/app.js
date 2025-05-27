1fbaf4496497f1c9cbd692260da45a579d12e624
Giovanni <giovanni_ge@hotmail.com>
2025-05-27 13:25:45 -0300

    feat: add

--- node-1/src/app.js
+++ node-1/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-
 const app = express();
+app.use(express.json());
 
 const livros = [
     {
@@ -15,7 +15,14 @@ const livros = [
         id: 3,
         titulo: "Novo teste"
     }
-]
+];
+
+// funções auxiliares
+function buscaLivros(id) {
+    return livros.findIndex((livro) => {
+        return livro.id === Number(id);
+    });
+}
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de node Node.js");
@@ -23,6 +30,23 @@ app.get("/", (req, res) => {
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
-})
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
+
 
 export default app;
