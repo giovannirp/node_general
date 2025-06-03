87f5a10f40471f680b86e4e540215f9196e98b47
Giovanni <giovanni_ge@hotmail.com>
2025-06-03 10:49:35 -0300

    feat

--- node-1/config/dbConnect.js
+++ node-1/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.bpxnjky.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0"
+    mongoose.connect("mongodb+srv://admin:admin123@cluster0.bpxnjky.mongodb.net/livraria?retryWrites=true&w=majority"
 )
 
     return mongoose.connection;
